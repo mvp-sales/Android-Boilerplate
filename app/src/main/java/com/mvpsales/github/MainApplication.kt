8b8befeb3ce11cd70462b28f42d3aade4a4b3b11
Marcus Sales <old.marcussales@gmail.com>
2025-01-23 20:52:42 -0300

    Adds initialization of Usercentrics SDK and basic usage

--- app/src/main/java/com/mvpsales/github/MainApplication.kt
+++ app/src/main/java/com/mvpsales/github/MainApplication.kt
@@ -1,7 +1,17 @@
 package com.mvpsales.github
 
 import android.app.Application
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsOptions
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication: Application()
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val options = UsercentricsOptions(settingsId = "gChmbFIdL")
+        Usercentrics.initialize(this, options)
+    }
+}
--- app/src/main/java/com/mvpsales/github/ui/MainFragment.kt
+++ app/src/main/java/com/mvpsales/github/ui/MainFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.mvpsales.github.databinding.FragmentMainBinding
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsBanner
+import com.usercentrics.sdk.UsercentricsServiceConsent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,5 +28,34 @@ class MainFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            showConsentBannerBtn.setOnClickListener {
+                Usercentrics.isReady({ status ->
+                    if (status.shouldCollectConsent) {
+                        // Show banner to collect consent
+                        showConsentBanner()
+                    } else {
+                        // Apply consent with status.consents
+                    }
+                }, { error ->
+                    // Handle non-localized error
+                    Snackbar.make(root, error.message ?: error.toString(), Snackbar.LENGTH_LONG).show()
+                })
+            }
+        }
+    }
+
+    private fun showConsentBanner() {
+        val banner = UsercentricsBanner(requireActivity())
+        banner.showFirstLayer() { userResponse ->
+            userResponse?.consents?.let { consents ->
+                applyConsents(consents)
+            }
+        }
+    }
+
+    private fun applyConsents(consents: List<UsercentricsServiceConsent>) {
+
     }
 }
