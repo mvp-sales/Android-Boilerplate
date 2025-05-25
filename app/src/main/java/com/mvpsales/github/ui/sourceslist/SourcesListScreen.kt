f0d40c18992b4f0bca36719a1d3f51cecbac8ea9
Marcus Sales <old.marcussales@gmail.com>
2025-05-25 11:47:02 +0100

    Updates SourcesListScreen

--- app/src/main/java/com/mvpsales/github/ui/sourceslist/SourcesListScreen.kt
+++ app/src/main/java/com/mvpsales/github/ui/sourceslist/SourcesListScreen.kt
@@ -1,13 +1,19 @@
 package com.mvpsales.github.ui.sourceslist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.BasicAlertDialog
@@ -24,12 +30,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.mvpsales.github.api.response.NewsSourceApiResponse
 import com.mvpsales.github.entities.NewsSource
 import com.mvpsales.github.entities.getLanguageCountryNames
-import com.mvpsales.github.ui.newssaved.NewsContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -66,7 +75,10 @@ fun SourcesListScreen(
                 }
             ) { padding ->
                 Column(modifier = Modifier.padding(padding)) {
-                    LazyColumn(modifier = Modifier.weight(1f)) {
+                    LazyColumn(
+                        modifier = Modifier.weight(1f),
+                        contentPadding = PaddingValues(8.dp)
+                    ) {
                         items(state.sources) { source ->
                             SourceContent(source)
                         }
@@ -90,13 +102,64 @@ fun SourcesListScreen(
 fun SourceContent(source: NewsSource) {
     Column(
         modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.White)
+            .border(BorderStroke(2.dp, Color.DarkGray), shape = RoundedCornerShape(8.dp))
             .padding(16.dp)
     ) {
-        Text(source.name)
-        Text(source.description)
-        Text(source.url)
-        Text(source.category)
-        val (language, country) = source.getLanguageCountryNames()
-        Text("${country}/Language: ${language}")
+        Text(
+            text = source.name,
+            style = MaterialTheme.typography.headlineSmall
+        )
+        val uriHandler = LocalUriHandler.current
+        Text(
+            modifier = Modifier.clickable {
+                    uriHandler.openUri(source.url)
+                }.padding(vertical = 4.dp),
+            text = source.url,
+            textAlign = TextAlign.End
+        )
+        Text(
+            modifier = Modifier.padding(top = 4.dp),
+            text = source.description,
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            modifier = Modifier.padding(vertical = 4.dp),
+            text = "Category: ${source.category}",
+            style = MaterialTheme.typography.labelSmall
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            val (language, country) = source.getLanguageCountryNames()
+            Text(
+                modifier = Modifier.weight(1f),
+                text = country,
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.labelSmall
+            )
+            Text(
+                modifier = Modifier.weight(1f),
+                text = "Language: $language",
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun SourceContentPreview() {
+    SourceContent(
+        NewsSource(
+            id = "abc-news",
+            name = "ABC News",
+            description = "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.",
+            url = "https://abcnews.go.com",
+            category = "general",
+            language = "en",
+            country = "us"
+        )
+    )
 }
